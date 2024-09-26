57c03ec262427acae8f04589b44625f33b7256ae
Shriniwas Kadam <shriniwaskadam25@gmail.com>
2024-09-26 22:12:39 +0530

    Tested DPM

--- scripts/server.py
+++ scripts/server.py
@@ -1,6 +1,7 @@
 import socket
 import numpy as np
 import struct
+import time
 
 def calculate_sine_offset(i, cycle_freq, ampl):
     return ampl * np.sin(2 * np.pi * cycle_freq * i)
@@ -11,8 +12,8 @@ def main():
     SERVER_PORT = 30000
     i = 0
     ctrl_freq = 125
-    weave_freq = 3
-    ampl = 0.01
+    weave_freq = 1
+    ampl = 10
     cycle_freq = weave_freq / ctrl_freq
 
     # Create a socket object
@@ -29,22 +30,35 @@ def main():
         client_socket, client_address = server_socket.accept()
         print(f"Connection accepted from {client_address}")
         while True:
-            msg = client_socket.recv(1024).decode()
-            if(msg=="capture"):
-                print("Capturing...")
-            elif(msg=="start_dpm"):
-                print("Starting DPM...")
-                while True:
-                    dy = calculate_sine_offset(i, cycle_freq, ampl)
-                    offs = struct.pack('<3f', 0.0, dy, 0.0)
-                    client_socket.sendall(offs)
-                    i = i + 1
-                    print(f"Sent offset {offs}")
-            elif not msg: 
-                print("Client has disconnected.")
-                break
-            else:
-                print(f"Message Received: {msg}")
+            data = client_socket.recv(4)
+            if data:
+                # Unpack the binary data to an integer
+                trigger = struct.unpack('<I', data)[0]  # '!I' for big-endian unsigned int, '<I' otherwise
+                if(trigger==42):
+                    print("Starting DPM...")
+                    while True:
+                        dy = calculate_sine_offset(i, cycle_freq, ampl)
+                        offs = struct.pack('<6f', 0.0, dy, 0.0, 0.0, 0.0, 0.0)
+                        client_socket.sendall(offs)
+                        i = i + 1
+                        time.sleep(0.008)
+            # msg = client_socket.recv(1024).decode()
+            # if(msg=="capture"):
+            #     print("Capturing...")
+            # elif(msg=="start_dpm"):
+            #     print("Starting DPM...")
+            #     while True:
+            #         dy = calculate_sine_offset(i, cycle_freq, ampl)
+            #         offs = struct.pack('<6f', 0.0, dy, 0.0, 0.0, 0.0, 0.0)
+            #         client_socket.sendall(offs)
+            #         i = i + 1
+            #         # print(f"Sent offset {offs}")
+            #         time.sleep(0.008)
+            # elif not msg: 
+            #     print("Client has disconnected.")
+            #     break
+            # else:
+            #     print(f"Message Received: {msg}")
     except ConnectionRefusedError:
         print("Connection failed. Make sure the server is running.")
     except KeyboardInterrupt:
